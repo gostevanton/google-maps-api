2ef776719a724ca93462f9fdc97f3e7b6cd83a01
gostevanton <gostevanton@yandex.ru>
2016-03-07 12:18:45 +0300

    Добавлен FromFragment и его разметка, для ввода данных "куда"

---
+++ GoogleMaps/app/src/main/java/com/example/anton/googlemaps/presantation/fragments/FromFragment.java
@@ -0,0 +1,21 @@
+package com.example.anton.googlemaps.presantation.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.anton.googlemaps.R;
+
+/**
+ * Created by Anton on 07.03.2016.
+ */
+public class FromFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_from, null);
+        return view;
+    }
+}
