ec2935cb5af5d024681fe0d7bb550a354f788290
gostevanton <gostevanton@yandex.ru>
2016-03-07 12:19:13 +0300

    Добавлен WhereFragment и его разметка, для ввода данных "куда"

---
+++ GoogleMaps/app/src/main/java/com/example/anton/googlemaps/presantation/fragments/WhereFragment.java
@@ -0,0 +1,21 @@
+package com.example.anton.googlemaps.presantation.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.anton.googlemaps.R;
+
+/**
+ * Created by Anton on 07.03.2016.
+ */
+public class WhereFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_where, null);
+        return view;
+    }
+}
